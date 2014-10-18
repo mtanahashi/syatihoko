96c0f7b608c591f8d0ee6972a91ef6140b8908c1
gem update rails --no-ri --no-rdoc <mk-t..203@c.vodafone.ne.jp>
2014-10-18 15:52:53 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = '7aac1f1d77ef69ed16ba9689ccc5b9539406b6724729026eaca0b08867710b17013903287ccd340bfd9de63c822a2f8e971634bb8f79e7061c63ad51ca5816f8'
